d7d2e546751b3c20be13f4e06130b6d49f8930a8
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-06-13 15:43:29 +0700

    update userRegistry add-event 13-6-2025

--- contracts/user/UserRegistry.sol
+++ contracts/user/UserRegistry.sol
@@ -2,18 +2,25 @@
 pragma solidity ^0.8.13;
 
 contract UserRegistry {
-    enum Role { None, Patient, Doctor }
+    enum Role {
+        None,
+        Patient,
+        Doctor
+    }
 
     mapping(address => Role) public roles;
+    event Registered(address indexed user, Role role);
 
     function registerAsPatient() public {
         require(roles[msg.sender] == Role.None, "Already registered");
         roles[msg.sender] = Role.Patient;
+        emit Registered(msg.sender, Role.Patient);
     }
 
     function registerAsDoctor() public {
         require(roles[msg.sender] == Role.None, "Already registered");
         roles[msg.sender] = Role.Doctor;
+        emit Registered(msg.sender, Role.Doctor);
     }
 
     function isPatient(address user) internal view returns (bool) {
@@ -23,5 +30,4 @@ contract UserRegistry {
     function isDoctor(address user) internal view returns (bool) {
         return roles[user] == Role.Doctor;
     }
- 
 }
