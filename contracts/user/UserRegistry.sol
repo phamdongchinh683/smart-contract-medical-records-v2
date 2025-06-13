9efb4e0fdae3454d22afc862b6f2835fccd642aa
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-06-13 13:59:52 +0700

    update file userRegistry 13-6-2025

---
+++ contracts/user/UserRegistry.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract UserRegistry {
+    enum Role { None, Patient, Doctor }
+
+    mapping(address => Role) public roles;
+
+    function registerAsPatient() public {
+        require(roles[msg.sender] == Role.None, "Already registered");
+        roles[msg.sender] = Role.Patient;
+    }
+
+    function registerAsDoctor() public {
+        require(roles[msg.sender] == Role.None, "Already registered");
+        roles[msg.sender] = Role.Doctor;
+    }
+
+    function isPatient(address user) internal view returns (bool) {
+        return roles[user] == Role.Patient;
+    }
+
+    function isDoctor(address user) internal view returns (bool) {
+        return roles[user] == Role.Doctor;
+    }
+ 
+}
